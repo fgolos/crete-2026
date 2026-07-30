fa731f78568fd03d94e5929600f81ee7b8837dda
fgolos <158696618+fgolos@users.noreply.github.com>
2026-07-31 00:45:19 +0300

    Refresh offline cache after mobile map fix

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_VERSION = 'crete-2026-v9';
+const CACHE_VERSION = 'crete-2026-v10';
 const SHELL_URL = new URL('./index.html', self.registration.scope).href;
 const CORE_ASSETS = [
   './',

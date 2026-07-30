769c613e1eff6cfdb4601db4188ef5105f9b0378
fgolos <158696618+fgolos@users.noreply.github.com>
2026-07-30 17:28:56 +0300

    Cache the August 13 to 15 story collection

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_VERSION = 'crete-2026-v6';
+const CACHE_VERSION = 'crete-2026-v7';
 const SHELL_URL = new URL('./index.html', self.registration.scope).href;
 const CORE_ASSETS = [
   './',
@@ -9,6 +9,7 @@ const CORE_ASSETS = [
   './itinerary-data.js',
   './itinerary-transport.js',
   './stories-data.js',
+  './stories-east-13-15.js',
   './app.js',
   './manifest.webmanifest',
   './icon.svg'
